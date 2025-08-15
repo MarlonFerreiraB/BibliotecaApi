2edf8ccbae0a54ed74efdb059935a2b1e48568f3
Marlon Ferreira <mferreirab03@gmail.com>
2025-08-15 16:59:50 -0300

    Adiciona modelo de livro com validações e anotações JPA

---
+++ src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/model/BookModel.java
@@ -0,0 +1,33 @@
+package io.github.marlon.bibliotecaapi.bibliotecaapi.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "book")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class BookModel {
+    private Integer actualyYear = LocalDate.now().getYear();
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @NotBlank(message = "Nao pode estar em branco")
+    @Size(max = 100, message = "Limite de caractere atingido.")
+    private String title;
+    @NotBlank(message = "Nao pode estar em branco")
+    @Min(value = 1900)
+    @Max(value = 2025)
+    @PastOrPresent
+    private Integer publicationYear;
+
+}
