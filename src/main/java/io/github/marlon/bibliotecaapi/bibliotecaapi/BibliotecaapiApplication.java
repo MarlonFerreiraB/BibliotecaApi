c4c2390139dc64562fe83cc4d98760eb25a0d059
Marlon Ferreira <mferreirab03@gmail.com>
2025-08-15 15:38:59 -0300

    Inicialização do projeto

---
+++ src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/BibliotecaapiApplication.java
@@ -0,0 +1,13 @@
+package io.github.marlon.bibliotecaapi.bibliotecaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaapiApplication.class, args);
+	}
+
+}
