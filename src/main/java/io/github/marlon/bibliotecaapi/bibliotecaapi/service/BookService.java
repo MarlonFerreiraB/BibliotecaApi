fa927ad1d27d004a1c7925b6eed93ce3f04973a2
Marlon Ferreira <mferreirab03@gmail.com>
2025-08-20 16:46:30 -0300

    Adiciona anotações de transação ao BookService para métodos de salvar, buscar e excluir livros

--- src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/service/BookService.java
+++ src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/service/BookService.java
@@ -4,6 +4,7 @@ import io.github.marlon.bibliotecaapi.bibliotecaapi.model.BookModel;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BookService {
@@ -11,15 +12,15 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
-
+    @Transactional
     public BookModel saveBook(BookModel bookModel){
         return bookRepository.save(bookModel);
     }
-
+    @Transactional(readOnly = true)
     public BookModel findByBook(String name){
-        return bookRepository.findByTitleContainingIgnoreCase(name);
+        return bookRepository.findByTitleIgnoreCase(name);
     }
-
+    @Transactional
     public void deleteBook(String name){
         bookRepository.deleteByTitle(name);
     }
