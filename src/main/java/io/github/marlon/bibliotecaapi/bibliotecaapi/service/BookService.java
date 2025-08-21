60255628326e001e9fd0fdfed2efbaa12898e6ef
Marlon Ferreira Batista <marlonfbdev@gmail.com>
2025-08-21 17:04:48 -0300

    Adiciona método para atualização de livros no BookService

--- src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/service/BookService.java
+++ src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/service/BookService.java
@@ -3,6 +3,7 @@ package io.github.marlon.bibliotecaapi.bibliotecaapi.service;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.model.BookModel;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +20,12 @@ public class BookService {
     @Transactional(readOnly = true)
     public BookModel findByBook(String name){
         return bookRepository.findByTitleIgnoreCase(name);
+    }
+    @Transactional
+    public BookModel updateBook(String id, BookModel bookModel){
+        if(!bookRepository.existsById(id)) return null;
+        bookModel.setId(id);
+        return bookRepository.save(bookModel);
     }
     @Transactional
     public void deleteBook(String name){
