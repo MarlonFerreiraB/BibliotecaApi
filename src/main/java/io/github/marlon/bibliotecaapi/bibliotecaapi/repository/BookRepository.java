ca0b61bb788aa713c7839a202b991b5ede746b4f
Marlon Ferreira <mferreirab03@gmail.com>
2025-08-30 16:09:13 -0300

    Add method to find books by author name for enhanced search functionality

--- src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/repository/BookRepository.java
+++ src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/repository/BookRepository.java
@@ -4,8 +4,11 @@ import io.github.marlon.bibliotecaapi.bibliotecaapi.model.BookModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<BookModel, String> {
     BookModel findByTitleIgnoreCase(String title);
     void deleteByTitle(String title);
+    List<BookModel> findByAuthor_NameContainingIgnoreCase(String name);
 }
