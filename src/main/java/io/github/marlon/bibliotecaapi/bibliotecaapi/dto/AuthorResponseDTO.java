05d6427897b02c9f02eed18c2c6c2dee3603f201
Marlon Ferreira <mferreirab03@gmail.com>
2025-08-30 16:08:07 -0300

    Remove unused fields and validation annotations from AuthorResponseDTO and BookResponseDTO for cleaner data transfer objects

--- src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/dto/AuthorResponseDTO.java
+++ src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/dto/AuthorResponseDTO.java
@@ -4,8 +4,6 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -13,11 +11,7 @@ import java.util.List;
 @NoArgsConstructor
 public class AuthorResponseDTO {
     private String id;
-    @NotBlank(message = "Este campo não pode ficar em branco.")
-    @Size(max = 100, message = "Limite de caractere atingido.")
     private String name;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 
     private List<BookResponseDTO> books;
 }
