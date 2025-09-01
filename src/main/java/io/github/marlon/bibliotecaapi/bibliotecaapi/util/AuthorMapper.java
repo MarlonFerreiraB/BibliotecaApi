4c402867c5252afc2b33194adc9cae34101b50b0
Marlon Ferreira <mferreirab03@gmail.com>
2025-09-01 15:40:09 -0300

    Add mapping for books in AuthorResponseDTO for improved data transfer

--- src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/util/AuthorMapper.java
+++ src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/util/AuthorMapper.java
@@ -7,12 +7,14 @@ import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.BookResponseDTO;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.model.AuthorModel;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.model.BookModel;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AuthorMapper {
 
     AuthorModel toEntity(AuthorCreationDTO authorCreationDTO);
 
+    @Mapping(target = "books", source = "books")
     AuthorResponseDTO toResponse(AuthorModel authorModel);
 
     BookResponseDTO toResponse(BookModel bookModel);
