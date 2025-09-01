edb50d2ad6877908060effd0b554c8f5e34a662e
Marlon Ferreira <mferreirab03@gmail.com>
2025-09-01 16:11:07 -0300

    Refactor BookMapper to include AuthorRepository and add update mapping for BookUpdateDTO

--- src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/util/BookMapper.java
+++ src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/util/BookMapper.java
@@ -2,17 +2,26 @@ package io.github.marlon.bibliotecaapi.bibliotecaapi.util;
 
 import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.BookCreationDTO;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.BookResponseDTO;
+import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.BookUpdateDTO;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.model.BookModel;
-import org.mapstruct.Mapper;
+import io.github.marlon.bibliotecaapi.bibliotecaapi.repository.AuthorRepository;
+import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface BookMapper {
+public abstract class BookMapper {
 
-    BookModel toEntity(BookCreationDTO bookCreationDTO);
+    @Autowired
+    AuthorRepository authorRepository;
 
-    BookResponseDTO toResponse(BookModel bookModel);
+    @Mapping(target = "author", expression = "java(authorRepository.findById(bookCreationDTO.getAuthorId()).orElse(null))" )
+    public abstract BookModel toEntity(BookCreationDTO bookCreationDTO);
 
-    List<BookResponseDTO> toResponseList(List<BookModel> bookModels);
+    public abstract BookResponseDTO toResponse(BookModel bookModel);
+
+    public abstract List<BookResponseDTO> toResponseList(List<BookModel> bookModels);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    public abstract void toEntityUpdate(BookUpdateDTO bookUpdateDTO, @MappingTarget BookModel bookModel);
 }
