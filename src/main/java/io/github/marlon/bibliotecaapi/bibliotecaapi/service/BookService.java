5993547f69feb7084edc480f64422616a67ec944
Marlon Ferreira <mferreirab03@gmail.com>
2025-09-02 16:15:55 -0300

    Refactor BookService methods for improved readability and consistency

--- src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/service/BookService.java
+++ src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/service/BookService.java
@@ -3,7 +3,6 @@ package io.github.marlon.bibliotecaapi.bibliotecaapi.service;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.BookCreationDTO;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.BookResponseDTO;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.BookUpdateDTO;
-import io.github.marlon.bibliotecaapi.bibliotecaapi.model.AuthorModel;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.model.BookModel;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.repository.AuthorRepository;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.repository.BookRepository;
@@ -25,25 +24,28 @@ public class BookService {
     BookMapper bookMapper;
 
     @Transactional
-    public BookResponseDTO saveBook(BookCreationDTO bookCreationDTO){
+    public BookResponseDTO saveBook(BookCreationDTO bookCreationDTO) {
         BookModel bookModel = bookMapper.toEntity(bookCreationDTO);
         bookRepository.save(bookModel);
         return bookMapper.toResponse(bookModel);
     }
+
     @Transactional(readOnly = true)
-    public BookResponseDTO findByBook(String name){
+    public BookResponseDTO findByBook(String name) {
         BookModel bookModel = bookRepository.findByTitleIgnoreCase(name);
         return bookMapper.toResponse(bookModel);
     }
+
     @Transactional
-    public BookResponseDTO updateBook(String id, BookUpdateDTO bookUpdateDTO){
+    public BookResponseDTO updateBook(String id, BookUpdateDTO bookUpdateDTO) {
         BookModel bookModel = bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Livro não encontrado"));
         bookMapper.toEntityUpdate(bookUpdateDTO, bookModel);
         BookModel updatedBook = bookRepository.save(bookModel);
         return bookMapper.toResponse(updatedBook);
     }
+
     @Transactional
-    public void deleteBook(String name){
-        bookRepository.deleteByTitle(name);
+    public void deleteBook(BookModel bookModel) {
+        bookRepository.delete(bookModel);
     }
 }
