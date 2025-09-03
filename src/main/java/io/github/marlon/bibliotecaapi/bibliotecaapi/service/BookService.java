5dfcb1cfa46df6ee55943821ba68189d6bda8bee
Marlon Ferreira <mferreirab03@gmail.com>
2025-09-03 17:06:39 -0300

    Add findyFilter method to BookService for enhanced book querying capabilities

--- src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/service/BookService.java
+++ src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/service/BookService.java
@@ -3,13 +3,18 @@ package io.github.marlon.bibliotecaapi.bibliotecaapi.service;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.BookCreationDTO;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.BookResponseDTO;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.BookUpdateDTO;
+import io.github.marlon.bibliotecaapi.bibliotecaapi.enums.GeneroEnum;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.model.BookModel;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.repository.AuthorRepository;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.repository.BookRepository;
+import io.github.marlon.bibliotecaapi.bibliotecaapi.repository.specs.BookSpecs;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.util.BookMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class BookService {
@@ -47,5 +52,21 @@ public class BookService {
     @Transactional
     public void deleteBook(BookModel bookModel) {
         bookRepository.delete(bookModel);
+    }
+
+    @Transactional(readOnly = true)
+    public List<BookModel> findyFilter(String title, GeneroEnum generoEnum, Integer anoPublicacao,String nomeAuthor ){
+        Specification<BookModel> specs = ((root, query, criteriaBuilder) -> criteriaBuilder.conjunction() );
+
+        if(title != null) specs = BookSpecs.titleEqual(title);
+
+
+        if(generoEnum != null) specs = BookSpecs.generoEqual(generoEnum);
+
+        if(anoPublicacao != null) specs = BookSpecs.dataDepublicacaoEqual(anoPublicacao);
+
+        if(nomeAuthor != null) specs = BookSpecs.AutorjoinLivro(nomeAuthor);
+
+        return bookRepository.findAll(specs);
     }
 }
