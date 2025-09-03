7903dfceebc835996fffab62f9ca9068e8295a2f
Marlon Ferreira <mferreirab03@gmail.com>
2025-09-03 17:06:50 -0300

    Add resultSearch endpoint to BookController for enhanced book filtering capabilities

--- src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/controller/BookController.java
+++ src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/controller/BookController.java
@@ -3,9 +3,11 @@ package io.github.marlon.bibliotecaapi.bibliotecaapi.controller;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.BookCreationDTO;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.BookResponseDTO;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.BookUpdateDTO;
+import io.github.marlon.bibliotecaapi.bibliotecaapi.enums.GeneroEnum;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.exceptions.BookNotFoundException;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.repository.BookRepository;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.service.BookService;
+import io.github.marlon.bibliotecaapi.bibliotecaapi.util.BookMapper;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/book")
@@ -22,6 +26,8 @@ public class BookController {
     BookService bookService;
     @Autowired
     BookRepository bookRepository;
+    @Autowired
+    BookMapper bookMapper;
 
     @PostMapping
     public ResponseEntity<BookResponseDTO> createBook(@RequestBody @Valid BookCreationDTO bookCreationDTO) {
@@ -36,17 +42,17 @@ public class BookController {
         return ResponseEntity.created(location).body(bookResponseDTO);
     }
 
-    @GetMapping
+   /* @GetMapping
     public ResponseEntity<BookResponseDTO> findBookByTitle(@RequestParam String name) {
         BookResponseDTO bookModel = bookService.findByBook(name);
         if (bookModel == null) throw new BookNotFoundException("Livro não encontrado");
         return ResponseEntity.ok(bookModel);
-    }
+    }*/
 
     @PutMapping("/{id}")
     public ResponseEntity<BookResponseDTO> updateBook(@PathVariable String id, @RequestBody BookUpdateDTO bookUpdateDTO) {
         BookResponseDTO bookUpdate = bookService.updateBook(id, bookUpdateDTO);
-        if (bookUpdate == null)  throw new BookNotFoundException("Livro não encontrado");
+        if (bookUpdate == null) throw new BookNotFoundException("Livro não encontrado");
         return ResponseEntity.ok(bookUpdate);
     }
 
@@ -57,4 +63,17 @@ public class BookController {
             return ResponseEntity.noContent().build();
         }).orElseGet(() -> ResponseEntity.notFound().build());
     }
+    @GetMapping
+    public ResponseEntity<List<BookResponseDTO>> resultSearch
+            (@RequestParam(value = "title", required = false) String title,
+             @RequestParam(value = "generoEnum", required = false) GeneroEnum generoEnum,
+             @RequestParam(value = "anoPublicacao", required = false) Integer publicationYear,
+             @RequestParam(value = "author", required = false) String nomeAuthor) {
+        var result = bookService.findyFilter(title, generoEnum, publicationYear, nomeAuthor);
+        var lista = result.stream()
+                .map(bookMapper::toResponse)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(lista);
+    }
+
 }
