fd0ff62ae367fa2164e3d4afaacb918ac69a0f9e
Marlon Ferreira <mferreirab03@gmail.com>
2025-09-03 17:07:01 -0300

    Refactor imports in BookMapper for improved code organization

--- src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/util/BookMapper.java
+++ src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/util/BookMapper.java
@@ -1,16 +1,10 @@
 package io.github.marlon.bibliotecaapi.bibliotecaapi.util;
 
-import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.AuthorResponseDTO;
-import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.BookCreationDTO;
-import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.BookResponseDTO;
-import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.BookUpdateDTO;
-import io.github.marlon.bibliotecaapi.bibliotecaapi.model.AuthorModel;
+import io.github.marlon.bibliotecaapi.bibliotecaapi.dto.*;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.model.BookModel;
 import io.github.marlon.bibliotecaapi.bibliotecaapi.repository.AuthorRepository;
 import org.mapstruct.*;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
 
 @Mapper(componentModel = "spring", uses = AuthorMapper.class)
 public abstract class BookMapper {
@@ -26,4 +20,6 @@ public abstract class BookMapper {
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     public abstract void toEntityUpdate(BookUpdateDTO bookUpdateDTO, @MappingTarget BookModel bookModel);
+
+
 }
