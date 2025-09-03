c39e93a93ffc59f7bd566483c04f3b7708f2109d
Marlon Ferreira <mferreirab03@gmail.com>
2025-09-03 15:26:15 -0300

    Remove unused imports in AuthorResponseDTO for improved code cleanliness

--- src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/dto/AuthorResponseDTO.java
+++ src/main/java/io/github/marlon/bibliotecaapi/bibliotecaapi/dto/AuthorResponseDTO.java
@@ -1,14 +1,13 @@
 package io.github.marlon.bibliotecaapi.bibliotecaapi.dto;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.List;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class AuthorResponseDTO {
     private String id;
     private String name;
